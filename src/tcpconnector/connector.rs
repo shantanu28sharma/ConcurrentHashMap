8e49d3ddb46a9574887987d640d24d0135563f64
shantanu28sharma <shantanu28sharma@gmail.com>
2020-12-23 11:03:30 +0530

    feat: unit tests and modular code

--- src/tcpconnector/connector.rs
+++ src/tcpconnector/connector.rs
@@ -1,4 +1,5 @@
 use crate::store;
+use std::borrow::Cow;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
@@ -18,25 +19,26 @@ impl Connector {
             }
         });
     }
+
     fn handle_client(mut stream: TcpStream, store: Arc<store::store::Store>) {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
         let iter = String::from_utf8_lossy(&buffer[..]);
-        let mut iter = iter.split_ascii_whitespace();
+        let response = Self::parse_string(iter, store);
+        stream.write(response.as_bytes());
+        stream.flush().unwrap();
+    }
+
+    fn parse_string(req: Cow<str>, store: Arc<store::store::Store>) -> String {
+        let mut iter = req.split_ascii_whitespace();
         match iter.next() {
             Some(val) => match val {
                 "GET" => match iter.next() {
                     Some(val) => match store.get(String::from(val)) {
-                        Ok(val) => {
-                            stream.write(val.as_bytes()).unwrap();
-                        }
-                        Err(e) => {
-                            stream.write("Error: Key does not exist".as_bytes());
-                        }
+                        Ok(val) => val,
+                        Err(e) => String::from("Error: Key does not exist"),
                     },
-                    None => {
-                        stream.write("Error: malformed input".as_bytes());
-                    }
+                    None => String::from("Error: Malfromed Input"),
                 },
                 "UPDATE" => {
                     let mut key = "";
@@ -46,7 +48,7 @@ impl Connector {
                             key = _key;
                         }
                         None => {
-                            stream.write("Error: malformed input".as_bytes());
+                            return String::from("Error: Malfromed Input");
                         }
                     }
                     match iter.next() {
@@ -54,30 +56,20 @@ impl Connector {
                             val = _val;
                         }
                         None => {
-                            stream.write("Error: malformed input".as_bytes());
+                            return String::from("Error: Malfromed Input");
                         }
                     }
                     match store.update(String::from(key), String::from(val)) {
-                        Ok(val) => {
-                            stream.write("Success".as_bytes()).unwrap();
-                        }
-                        Err(e) => {
-                            stream.write("Error: Key does not exist".as_bytes());
-                        }
+                        Ok(val) => String::from("Success"),
+                        Err(e) => String::from("Error: Key does not exist"),
                     }
                 }
                 "DELETE" => match iter.next() {
                     Some(val) => match store.delete(val) {
-                        Ok(val) => {
-                            stream.write(val.as_bytes()).unwrap();
-                        }
-                        Err(e) => {
-                            stream.write("Error: Key does not exist".as_bytes());
-                        }
+                        Ok(val) => val,
+                        Err(e) => String::from("Error: Key does not exist"),
                     },
-                    None => {
-                        stream.write("Error: malformed input".as_bytes());
-                    }
+                    None => String::from("Error: Malfromed Input"),
                 },
                 "INSERT" => {
                     let mut key = "";
@@ -87,7 +79,7 @@ impl Connector {
                             key = _key;
                         }
                         None => {
-                            stream.write("Error: malformed input".as_bytes());
+                            return String::from("Error: Malfromed Input");
                         }
                     }
                     match iter.next() {
@@ -95,39 +87,80 @@ impl Connector {
                             val = _val;
                         }
                         None => {
-                            stream.write("Error: malformed input".as_bytes());
+                            return String::from("Error: Malfromed Input");
                         }
                     }
                     match store.insert(String::from(key), String::from(val), 40) {
-                        Ok(val) => {
-                            stream.write("Success".as_bytes()).unwrap();
-                        }
-                        Err(e) => {
-                            stream.write("Error: Key does not exist".as_bytes());
-                        }
+                        Ok(val) => String::from("Success"),
+                        Err(e) => String::from("Error: Key does not exist"),
                     }
                 }
-                _ => {
-                    stream.write("Error: malformed input".as_bytes());
-                }
+                _ => String::from("Error: Unknown Method"),
             },
-            _ => {
-                stream.write("Error: malformed input".as_bytes());
-            }
+            _ => String::from("Error: Malfromed Input"),
         }
-        stream.flush().unwrap();
     }
 }
 
-#[cfg(test)] 
-mod tests{
+#[cfg(test)]
+mod tests {
     use super::*;
+
     #[test]
     fn connector_test() {
         let store = Arc::new(store::store::Store::new());
         Connector::run(store.clone());
         assert_eq!(1, 1);
     }
+
+    #[test]
+    fn parse_test_insert() {
+        let store = Arc::new(store::store::Store::new());
+        let request = Cow::from("INSERT hello world");
+        let response = Connector::parse_string(request, store);
+        assert_eq!(response, String::from("Success"));
+    }
+
+    #[test]
+    fn parse_test_update() {
+        let store = Arc::new(store::store::Store::new());
+        store.insert(String::from("hello"), String::from("world"), 40);
+        let request = Cow::from("UPDATE hello corona");
+        let response = Connector::parse_string(request, store.clone());
+        let request = Cow::from("GET hello");
+        let response = Connector::parse_string(request, store.clone());
+        assert_eq!(response, String::from("corona"));
+    }
+
+    #[test]
+    fn parse_test_insert_bad_input() {
+        let store = Arc::new(store::store::Store::new());
+        let request = Cow::from("INSERT hello");
+        let response = Connector::parse_string(request, store);
+        assert_eq!(response, String::from("Error: Malfromed Input"));
+    }
+    
+    #[test]
+    fn parse_test_unknown_method() {
+        let store = Arc::new(store::store::Store::new());
+        let request = Cow::from("CRUD hello world");
+        let response = Connector::parse_string(request, store);
+        assert_eq!(response, String::from("Error: Unknown Method"));
+    }
+
+    #[test]
+    fn parse_test_update_bad_input() {
+        let store = Arc::new(store::store::Store::new());
+        let request = Cow::from("UPDATE hello");
+        let response = Connector::parse_string(request, store);
+        assert_eq!(response, String::from("Error: Malfromed Input"));
+    }
+
+    #[test]
+    fn parse_test_get_unknown_key() {
+        let store = Arc::new(store::store::Store::new());
+        let request = Cow::from("GET hello");
+        let response = Connector::parse_string(request, store);
+        assert_eq!(response, String::from("Error: Key does not exist"));
+    }
 }
-
-
